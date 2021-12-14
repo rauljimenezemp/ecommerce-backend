cb398ba844430abed5e649ef00d39c12a974e82e
Raúl JR <83794310+rauljimenezemp@users.noreply.github.com>
2021-12-14 16:52:58 -0600

    Estructura básica del servidor.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log('Hola mundo!');
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  return res.send('Hola!');
+});
+
+app.listen(PORT, () =>
+  console.log('Servidor corriendo en http://localhost:' + PORT)
+);
