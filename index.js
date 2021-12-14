eabb11c726386048112ae410ffbe82661bf313e0
Raúl JR <83794310+rauljimenezemp@users.noreply.github.com>
2021-12-14 17:34:04 -0600

    Agregar rutas dummy para Auth y products.

--- index.js
+++ index.js
@@ -9,6 +9,56 @@ app.get('/', (req, res) => {
   return res.send('Hola!');
 });
 
+
+//AUTH Usuarios
+//Register - POST
+app.post('/api/v1/auth/register', (req, res) => {
+  return res.send({message: 'Hola desde register!'});
+});
+
+//Login - POST
+app.post('/api/v1/auth/login', (req, res) => {
+  return res.send({ message: 'Hola desde login!'});
+});
+
+//Logout - POST
+app.post('/api/v1/auth/logout', (req, res) => {
+  return res.send({ message: 'Hola desde logout!'});
+});
+
+//CRUD - Productos
+//Create - POST
+app.post('/api/v1/products', (req, res) => {
+  return res.send({ message: 'Hola desde CREATE products!'});
+});
+
+//Read All - GET
+app.get('/api/v1/products', (req, res) => {
+  return res.send( {message: 'Hola desde READ ALL products!'});
+});
+
+//Read One - GET
+app.get('/api/v1/products/:id', (req, res) => {
+  return res.send( {message: 'Hola desde READ ONE products!'});
+});
+
+//Update - PUT
+app.put('/api/v1/products/:id', (req, res) => {
+  return res.send({message: 'Hola desde UPDATE products!'});
+});
+
+//Delete - DELETE
+app.delete('/api/v1/products/:id', (req, res) => {
+  return res.send({message: 'Hola desde DELETE products!'});
+});
+
+//Destroy - DELETE
+app.delete('/api/v1/products/:id/destroy', (req, res) => {
+  return res.send({message: 'Hola desde DESTROY products!'});
+});
+
+
+
 app.listen(PORT, () =>
   console.log('Servidor corriendo en http://localhost:' + PORT)
 );
