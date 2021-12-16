6212c5b651dfcfd19a7ee6247de0f1633c65a39e
Raúl JR <83794310+rauljimenezemp@users.noreply.github.com>
2021-12-15 22:29:58 -0600

    Implementar modelos de autenticación y de productos en comntroladores.

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,6 +1,17 @@
+const { AuthModel } = require('../models')
+
 const register = (req, res) => {
-  return res.send({message: 'Hola desde register!'});
-}
+  const {body} = req;
+  return AuthModel.create(body)
+    .then(newUser => {
+      res.status(200).send({ message: 'Usuario creado!', newUser});
+    })
+    .catch(err => {
+      res
+        .status(400)
+        .send({message: 'Error creando usuario!', error: err.message});
+      });
+};
 
 const login = (req, res) => {
   return res.send({ message: 'Hola desde login!'});
